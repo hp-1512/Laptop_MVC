b1ece7585a01179f28149c07e9baf46fc16d47bb
Harshkumar A Patel <pci186@server7.com>
2023-02-10 18:21:40 +0530

    MVC loginpage

---
+++ CI-Platform_web/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform_web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
