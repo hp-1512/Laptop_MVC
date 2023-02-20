080dbdeb24726af6fbf47572bced8d318ee98edf
Harshkumar A Patel <pci186@server7.com>
2023-02-20 18:29:49 +0530

    No mission page

--- CI-Platform_web/Controllers/HomeController.cs
+++ CI-Platform_web/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace CI_Platform_web.Controllers
         {
             return View();
         }
-
+        public IActionResult noMission()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
